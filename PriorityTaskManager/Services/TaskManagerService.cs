91a77216fcec97358c4cfcdfa67c1ad8e8237d18
Nicholas Santos <santos_nick@outlook.com>
2025-09-15 12:00:42 -0700

    Manual rearrangement

--- PriorityTaskManager/Services/TaskManagerService.cs
+++ PriorityTaskManager/Services/TaskManagerService.cs
@@ -47,112 +47,6 @@ namespace PriorityTaskManager.Services
             _urgencyService.CalculateUrgencyForAllTasks(_tasks);
         }
 
-        private void SaveTasks()
-        {
-            var data = new
-            {
-                Tasks = _tasks,
-                NextId = _nextId
-            };
-
-            File.WriteAllText(_filePath, JsonSerializer.Serialize(data));
-        }
-
-        private void LoadTasks()
-        {
-            if (File.Exists(_filePath))
-            {
-                try
-                {
-                    var json = File.ReadAllText(_filePath);
-                    var data = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json);
-
-                    if (data != null && data.ContainsKey("Tasks") && data.ContainsKey("NextId"))
-                    {
-                        var rawTasks = data["Tasks"].GetRawText();
-                        var loadedTasks = new List<TaskItem>();
-                        try
-                        {
-                            loadedTasks = JsonSerializer.Deserialize<List<TaskItem>>(rawTasks) ?? new List<TaskItem>();
-                        }
-                        catch (ArgumentException)
-                        {
-                            // Skip all tasks if deserialization fails due to invalid title
-                            loadedTasks = new List<TaskItem>();
-                        }
-                        // Initialize EffectiveImportance for loaded tasks
-                        foreach (var t in loadedTasks)
-                        {
-                            t.EffectiveImportance = t.Importance;
-                        }
-                        _tasks = loadedTasks;
-                        _nextId = data["NextId"].GetInt32();
-                    }
-                }
-                catch
-                {
-                    // If any error occurs, skip loading tasks
-                    _tasks = new List<TaskItem>();
-                    _nextId = 1;
-                }
-            }
-        }
-
-        private void LoadLists()
-        {
-            if (File.Exists(_listFilePath))
-            {
-                try
-                {
-                    var json = File.ReadAllText(_listFilePath);
-                    var data = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json);
-                    if (data != null && data.ContainsKey("Lists"))
-                    {
-                        var rawLists = data["Lists"].GetRawText();
-                        _lists = JsonSerializer.Deserialize<List<TaskList>>(rawLists) ?? new List<TaskList>();
-                        if (_lists.Count > 0)
-                        {
-                            _nextListId = _lists.Max(l => l.Id) + 1;
-                        }
-                        else
-                        {
-                            _nextListId = 1;
-                        }
-                        if (data.ContainsKey("NextListId"))
-                        {
-                            _nextListId = Math.Max(_nextListId, data["NextListId"].GetInt32());
-                        }
-                    }
-                    else
-                    {
-                        _lists = new List<TaskList>();
-                        _nextListId = 1;
-                    }
-                }
-                catch
-                {
-                    _lists = new List<TaskList>();
-                    _nextListId = 1;
-                }
-            }
-            else
-            {
-                var defaultList = new TaskList { Id = _nextListId++, Name = "General", SortOption = SortOption.Default };
-                _lists.Add(defaultList);
-                SaveLists();
-            }
-        }
-
-        private void SaveLists()
-        {
-            var data = new
-            {
-                Lists = _lists,
-                NextListId = _nextListId
-            };
-            File.WriteAllText(_listFilePath, JsonSerializer.Serialize(data));
-        }
-
         /// <summary>
         /// Adds a new task to the collection and saves the changes.
         /// </summary>
@@ -191,41 +85,6 @@ namespace PriorityTaskManager.Services
         }
 
         /// <summary>
-        /// Checks if adding the given dependencies to the specified task would create a circular dependency.
-        /// </summary>
-        /// <param name="taskId">The ID of the task being updated.</param>
-        /// <param name="newDependencies">The list of proposed new dependencies.</param>
-        /// <returns>True if a cycle would be created; otherwise, false.</returns>
-        private bool WouldCreateCycle(int taskId, List<int> newDependencies)
-        {
-            var visited = new HashSet<int>();
-            foreach (var depId in newDependencies)
-            {
-                if (DetectCycleRecursive(taskId, depId, visited))
-                    return true;
-            }
-            return false;
-        }
-
-        private bool DetectCycleRecursive(int originalTaskId, int currentId, HashSet<int> visited)
-        {
-            if (currentId == originalTaskId)
-                return true;
-            if (visited.Contains(currentId))
-                return false;
-            visited.Add(currentId);
-            var currentTask = _tasks.Find(t => t.Id == currentId);
-            if (currentTask == null)
-                return false;
-            foreach (var depId in currentTask.Dependencies)
-            {
-                if (DetectCycleRecursive(originalTaskId, depId, visited))
-                    return true;
-            }
-            return false;
-        }
-
-        /// <summary>
         /// Updates an existing task with new details.
         /// </summary>
         /// <param name="updatedTask">The updated task object.</param>
@@ -254,41 +113,6 @@ namespace PriorityTaskManager.Services
             SaveTasks();
 
             return true;
-        }
-
-        /// <summary>
-        /// Checks if updating a task's dependencies would create a circular dependency.
-        /// </summary>
-        private bool WouldCreateCircularDependency(int taskId, List<int> newDependencies)
-        {
-            var visited = new HashSet<int>();
-            foreach (var depId in newDependencies)
-            {
-                if (HasCycle(taskId, depId, visited))
-                    return true;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Recursively checks for cycles in the dependency graph.
-        /// </summary>
-        private bool HasCycle(int originalTaskId, int currentId, HashSet<int> visited)
-        {
-            if (currentId == originalTaskId)
-                return true;
-            if (visited.Contains(currentId))
-                return false;
-            visited.Add(currentId);
-            var currentTask = _tasks.Find(t => t.Id == currentId);
-            if (currentTask == null)
-                return false;
-            foreach (var depId in currentTask.Dependencies)
-            {
-                if (HasCycle(originalTaskId, depId, visited))
-                    return true;
-            }
-            return false;
         }
 
         /// <summary>
@@ -419,6 +243,182 @@ namespace PriorityTaskManager.Services
                 existingList.SortOption = updatedList.SortOption;
                 SaveLists();
             }
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(_filePath))
+            {
+                try
+                {
+                    var json = File.ReadAllText(_filePath);
+                    var data = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json);
+
+                    if (data != null && data.ContainsKey("Tasks") && data.ContainsKey("NextId"))
+                    {
+                        var rawTasks = data["Tasks"].GetRawText();
+                        var loadedTasks = new List<TaskItem>();
+                        try
+                        {
+                            loadedTasks = JsonSerializer.Deserialize<List<TaskItem>>(rawTasks) ?? new List<TaskItem>();
+                        }
+                        catch (ArgumentException)
+                        {
+                            // Skip all tasks if deserialization fails due to invalid title
+                            loadedTasks = new List<TaskItem>();
+                        }
+                        // Initialize EffectiveImportance for loaded tasks
+                        foreach (var t in loadedTasks)
+                        {
+                            t.EffectiveImportance = t.Importance;
+                        }
+                        _tasks = loadedTasks;
+                        _nextId = data["NextId"].GetInt32();
+                    }
+                }
+                catch
+                {
+                    // If any error occurs, skip loading tasks
+                    _tasks = new List<TaskItem>();
+                    _nextId = 1;
+                }
+            }
+        }
+
+        private void SaveTasks()
+        {
+            var data = new
+            {
+                Tasks = _tasks,
+                NextId = _nextId
+            };
+
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(data));
+        }
+
+        private void LoadLists()
+        {
+            if (File.Exists(_listFilePath))
+            {
+                try
+                {
+                    var json = File.ReadAllText(_listFilePath);
+                    var data = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json);
+                    if (data != null && data.ContainsKey("Lists"))
+                    {
+                        var rawLists = data["Lists"].GetRawText();
+                        _lists = JsonSerializer.Deserialize<List<TaskList>>(rawLists) ?? new List<TaskList>();
+                        if (_lists.Count > 0)
+                        {
+                            _nextListId = _lists.Max(l => l.Id) + 1;
+                        }
+                        else
+                        {
+                            _nextListId = 1;
+                        }
+                        if (data.ContainsKey("NextListId"))
+                        {
+                            _nextListId = Math.Max(_nextListId, data["NextListId"].GetInt32());
+                        }
+                    }
+                    else
+                    {
+                        _lists = new List<TaskList>();
+                        _nextListId = 1;
+                    }
+                }
+                catch
+                {
+                    _lists = new List<TaskList>();
+                    _nextListId = 1;
+                }
+            }
+            else
+            {
+                var defaultList = new TaskList { Id = _nextListId++, Name = "General", SortOption = SortOption.Default };
+                _lists.Add(defaultList);
+                SaveLists();
+            }
+        }
+
+        private void SaveLists()
+        {
+            var data = new
+            {
+                Lists = _lists,
+                NextListId = _nextListId
+            };
+            File.WriteAllText(_listFilePath, JsonSerializer.Serialize(data));
+        }
+
+        /// <summary>
+        /// Checks if adding the given dependencies to the specified task would create a circular dependency.
+        /// </summary>
+        /// <param name="taskId">The ID of the task being updated.</param>
+        /// <param name="newDependencies">The list of proposed new dependencies.</param>
+        /// <returns>True if a cycle would be created; otherwise, false.</returns>
+        private bool WouldCreateCycle(int taskId, List<int> newDependencies)
+        {
+            var visited = new HashSet<int>();
+            foreach (var depId in newDependencies)
+            {
+                if (DetectCycleRecursive(taskId, depId, visited))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool DetectCycleRecursive(int originalTaskId, int currentId, HashSet<int> visited)
+        {
+            if (currentId == originalTaskId)
+                return true;
+            if (visited.Contains(currentId))
+                return false;
+            visited.Add(currentId);
+            var currentTask = _tasks.Find(t => t.Id == currentId);
+            if (currentTask == null)
+                return false;
+            foreach (var depId in currentTask.Dependencies)
+            {
+                if (DetectCycleRecursive(originalTaskId, depId, visited))
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if updating a task's dependencies would create a circular dependency.
+        /// </summary>
+        private bool WouldCreateCircularDependency(int taskId, List<int> newDependencies)
+        {
+            var visited = new HashSet<int>();
+            foreach (var depId in newDependencies)
+            {
+                if (HasCycle(taskId, depId, visited))
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Recursively checks for cycles in the dependency graph.
+        /// </summary>
+        private bool HasCycle(int originalTaskId, int currentId, HashSet<int> visited)
+        {
+            if (currentId == originalTaskId)
+                return true;
+            if (visited.Contains(currentId))
+                return false;
+            visited.Add(currentId);
+            var currentTask = _tasks.Find(t => t.Id == currentId);
+            if (currentTask == null)
+                return false;
+            foreach (var depId in currentTask.Dependencies)
+            {
+                if (HasCycle(originalTaskId, depId, visited))
+                    return true;
+            }
+            return false;
         }
     }
 }
